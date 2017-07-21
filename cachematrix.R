7ac09d1df53239a8f7d67c6d7c976fe52d0ca735
sherifffruitfly <sherifffruitfly@gmail.com>
2017-07-21 08:58:11 -0700

    This one going up for peer review.

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,13 @@
-## Put comments here that give an overall description of what your
-## 
+## This set of functions sets up an OOP-style matrix object, with methods for 
+## setting & getting the matrix and its inverse.
+##
+## The course-supplied functions are restricted to working on on deep copies of
+## the matrix object. The version i supply works on these, but ALSO on mere shallow copies.
+##
+## If needed, cacheSolve.old is the weaker version the course suggested.
 
-## Write a short comment describing this function
 
+## makeCacheMatrix
 makeCacheMatrix <- function(mat = matrix())
 {
   cachedInv <- NULL
@@ -27,10 +32,6 @@ makeCacheMatrix <- function(mat = matrix())
   getInv <- function(inv)
   {
     cachedInv
-    #have v2 of this seach parent environment for all identical cacheMatrix objects for THEIR cached inverse
-    #this would mean refactoring the code and make a SearchEnvironmentForInverse(mat) function
-    #should i build a call to cacheSolve into this too? basically guaranteeing that getInv always returns inverse without
-    #explicit call to solve?
   }
   
   getOriginal <- function(inv)
@@ -45,6 +46,87 @@ makeCacheMatrix <- function(mat = matrix())
     , getInv = getInv
     , getOriginal = getOriginal
   )
+}
+
+
+## cacheSolve improves cacheSolve.old by working on shallow copies instead of only deep copies
+## cacheSolve searches cache environment for any cacheMatrix objects matching the input, and gets their cached inverse.
+## cacheSole also assigns inverse to all matching parent cacheMatrix objects, minimizing the "need" to ever invert that matrix again.
+
+cacheSolve <- function(mat, verbose=FALSE,  ...)
+{
+  if (verbose) message("Solve starting in verbose mode.")
+  
+  # check current matrix for cached inverse
+  inv <- mat$getInv()
+  
+  if(is.null(inv))
+  {
+    if (verbose) message("No cached inverse found for input. Searching for other matching objects...")
+    
+    # searches parent environment (cache) for identical cached matrix & inverse
+    cache_obs <- ls(parent.env(environment()))
+    obs_wo_inverse <- c()
+    
+    # check all the objects, looking for cacheMatrix objects. there should be a simpler way.
+    if (length(cache_obs) > 0)
+    {
+      for (i in 1:length(cache_obs))
+      {
+        # objects of length 5 (the makecachedmatrix list length) are what we want
+        if (length(get(cache_obs[i])) == 5)
+        {
+          # elements 4 and 5 have known names
+          if (!is.null(names(get(cache_obs[i])[4])[1]) & !is.null(names(get(cache_obs[i])[5])[1]))
+          {
+            if (names(get(cache_obs[i])[4])[1] == "getInv" & names(get(cache_obs[i])[5])[1] == "getOriginal")
+            {
+              if (verbose) message("Checking object ", cache_obs[i], " for equality with input.")
+              
+              # check cached object for value identity
+              if (all(get(cache_obs[i])$getOriginal() == mat$get()))
+              {
+                # we have same cached matrix - now check for cached inverse
+                if (verbose) message("Object ",  cache_obs[i], " matches input. Looking for cached inverse.")
+                inv <- get(cache_obs[i])$getInv()
+                if(!is.null(inv))
+                {
+                  if (verbose) message("Getting cached inverse from object ", cache_obs[i])
+                  mat$setInv(inv)
+                  return(inv)
+                } else
+                {
+                  if (verbose) message("Object ", cache_obs[i], " has no inverse cached.")
+                  obs_wo_inverse <- c(obs_wo_inverse, cache_obs[i])
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    
+    # we didn't hit that return in the innermost if above, so calculate the inverse on current object
+    if (verbose) message("No matching cached inverse found. Calculating inverse for input, and caching it on all matching existing objects")
+    data <- mat$get()
+    inv <- solve(data, ...)
+    mat$setInv(inv)
+    
+    # now we set the inverse for all identical cacheMatrix objects that were found without inverses, so we don't have to go thru this again on them.
+    if (length(obs_wo_inverse) > 0)
+    {
+      for (i in 1:length(obs_wo_inverse))
+      {
+        get(obs_wo_inverse[i])$setInv(inv)
+      }
+    }
+    
+    return(inv)
+  } else
+  {
+    if (verbose) message("Getting cached inverse from current object.")
+    return(inv)
+  }
 }
 
 
@@ -67,77 +149,4 @@ cacheSolve.old <- function(mat, ...)
   inv <- solve(data, ...)
   mat$setInv(inv)
   inv
-}
-
-
-## cacheSolve improves cacheSolve.old by working on shallow copies instead of only deep copies
-## cacheSolve searches cache environment for any cacheMatrix objects matching the input, and gets their input
-
-cacheSolve <- function(mat, ...)
-{
-  # check current matrix for cached inverse
-  inv <- mat$getInv()
-  
-  if(is.null(inv))
-  {
-    # searches parent environment (cache) for identical cached matrix & inverse
-    cache_obs <- ls(parent.env(environment()))
-    obs_wo_inverse <- c()
-    
-    # check all the objects, looking for cacheMatrix objects. there should be a simpler way.
-    if (length(cache_obs) > 0)
-    {
-      for (i in 1:length(cache_obs))
-      {
-        # objects of length 5 (the makecachedmatrix list length) are what we want
-        if (length(get(cache_obs[i])) == 5)
-        {
-          # elements 4 and 5 have known names
-          if (!is.null(names(get(cache_obs[i])[4])[1]) & !is.null(names(get(cache_obs[i])[5])[1]))
-          {
-            if (names(get(cache_obs[i])[4])[1] == "getInv" & names(get(cache_obs[i])[5])[1] == "getOriginal")
-            {
-              message("Checking object ", cache_obs[i], " for equality with input.")
-              
-              # check cached object for value identity
-              if (all(get(cache_obs[i])$getOriginal() == mat$get()))
-              {
-                # we have same cached matrix - now check for cached inverse
-                message("Object ",  cache_obs[i], " matches input. Looking for cached inverse.")
-                inv <- get(cache_obs[i])$getInv()
-                if(!is.null(inv))
-                {
-                  message("Getting cached inverse from object ", cache_obs[i])
-                  mat$setInv(inv)
-                  return(inv)
-                } else
-                {
-                  message("Object ", cache_obs[i], " has no inverse cached.")
-                          obs_wo_inverse <- c(obs_wo_inverse, cache_obs[i])
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-    
-    # we didn't hit that return in the innermost if above, so calculate the inverse on current object
-    message("Calculating inverse for input, and assigning it to caches for all matching existing objects")
-    data <- mat$get()
-    inv <- solve(data, ...)
-    mat$setInv(inv)
-    
-    # now we set the inverse for all identical cacheMatrix objects that were found without inverses, so we don't have to go thru this again on them.
-    for (i in 1:length(obs_wo_inverse))
-    {
-      get(obs_wo_inverse[i])$setInv(inv)
-    }
-    
-    return(inv)
-  } else
-  {
-    message("Getting cached inverse from current object.")
-    return(inv)
-  }
 }
--- units_cacheSolve.R
+++ units_cacheSolve.R
@@ -1,61 +1,175 @@
 # Matrix inverse unit tests
 
 
-# Unit 1
-# accuracy test 1
+# Unit 0
+# display test 1
+
 rm(list = ls()) # clean the environment
 source('C:/cdjProgramming/coursera/r/week3/ProgrammingAssignment2/cachematrix.R') # load functions to be tested
 
+verboseQ <- FALSE
 testMatrix <- rbind(c(1, -1/4), c(-1/4, 1))
 
 testMatrixInv <- solve(testMatrix)
-cacheMatrixInv <- cacheSolve(makeCacheMatrix(testMatrix))
+cacheMatrixInv <- cacheSolve(makeCacheMatrix(testMatrix), verboseQ)
+
+message("Original: ")
+print(testMatrix)
+message("Solve: ")
+print(testMatrixInv)
+message("cacheSolve: ")
+print(cacheMatrixInv)
+
+message("Unit 0, disp1: Test complete.")
+
+
+# Unit 1
+# accuracy test 1
+
+rm(list = ls()) # clean the environment
+source('C:/cdjProgramming/coursera/r/week3/ProgrammingAssignment2/cachematrix.R') # load functions to be tested
+
+verboseQ <- FALSE
+testMatrix <- rbind(c(1, -1/4), c(-1/4, 1))
+
+testMatrixInv <- solve(testMatrix)
+cacheMatrixInv <- cacheSolve(makeCacheMatrix(testMatrix), verboseQ)
 
 if(all(testMatrixInv == cacheMatrixInv))
 {
-  print("Unit 1, acc1: Success")
+  message("Unit 1, acc1: Success")
 } else
 {
-  print("Unit 1, acc1: Fail")
+  message("Unit 1, acc1: Fail")
 }
 
 
 # Unit 2
-# accuracy test 2
+# accuracy test 2 - Vandermonde
+
 rm(list = ls()) # clean the environment
 source('C:/cdjProgramming/coursera/r/week3/ProgrammingAssignment2/cachematrix.R') # load functions to be tested
 
+verboseQ <- TRUE
 testMatrix <- rbind(c(1, 1, 1, 1), c(2, 3, 4, 5), c(4, 9, 16, 25), c(8, 27, 64, 125))
 
 testMatrixInv <- solve(testMatrix)
-cacheMatrixInv <- cacheSolve(makeCacheMatrix(testMatrix))
+cacheMatrixInv <- cacheSolve(makeCacheMatrix(testMatrix), verboseQ)
 
 if (all(testMatrixInv == cacheMatrixInv))
 {
-  print("Unit 2, acc1: Success")
+  message("Unit 2, acc2: Success")
 } else
 {
-  print("Unit 2, acc2: Fail")
+  message("Unit 2, acc2: Fail")
 }
 
 
 # Unit 3
-# cache test 1
+# cache test 1 - Vandermonde
+
 rm(list = ls()) # clean the environment
 source('C:/cdjProgramming/coursera/r/week3/ProgrammingAssignment2/cachematrix.R') # load functions to be tested
 
+verboseQ <- FALSE
 testMatrix <- rbind(c(1, 1, 1, 1), c(2, 3, 4, 5), c(4, 9, 16, 25), c(8, 27, 64, 125))
 
 testMatrixInv <- solve(testMatrix)
 cm <- makeCacheMatrix(testMatrix)
-cacheMatrixInv <- cacheSolve(cm)
-cacheMatrixInv <- cacheSolve(cm)
+cacheMatrixInv <- cacheSolve(cm, verboseQ)
+cacheMatrixInv <- cacheSolve(cm, verboseQ)
 
 if (all(testMatrixInv == cacheMatrixInv))
 {
-  print("Unit 2, cache1: Success")
+  message("Unit 3, cache1: Success")
 } else
 {
-  print("Unit 2, cache1: Fail")
+  message("Unit 3, cache1: Fail")
 }
+
+
+# unit 4
+# many test 1: integer matrices with integer inverses
+
+rm(list = ls()) # clean the environment
+source('C:/cdjProgramming/coursera/r/week3/ProgrammingAssignment2/cachematrix.R') # load functions to be tested
+
+verboseQ <- FALSE
+result <- c()
+maxruns <- 100
+peek <- sample(1:maxruns, 1, replace = TRUE)
+for (i in 1:maxruns)
+{
+  a <- sample(1:10, 1, replace = TRUE)
+  b <- sample(1:10, 1, replace = TRUE)
+  c <- sample(1:10, 1, replace = TRUE)
+  d <- sample(1:10, 1, replace = TRUE)
+  e <- sample(1:10, 1, replace = TRUE)
+  f <- sample(1:10, 1, replace = TRUE)
+  
+  factor1 <- rbind(c(1, a, b), c(0, 1, c), c(0, 0, 1))
+  factor2 <- rbind(c(1, 0, 0), c(d, 1, 0), c(e, f, 1))
+  
+  testMatrix <- factor1 %*% factor2
+  cm = makeCacheMatrix(testMatrix)
+  
+  testMatrixInv <- solve(testMatrix)
+  cacheMatrixInv <- cacheSolve(cm, verboseQ)
+  
+  # we just pick one of these to show the user
+  if (i == peek)
+  {
+    message("Unit 4: Peeking into run #", i)
+    message("Original: ")
+    print(testMatrix)
+    message("Solve: ")
+    print(testMatrixInv)
+    message("cacheSolve: ")
+    print(cacheMatrixInv)
+  }
+  
+  if(all(testMatrixInv == cacheMatrixInv) & all(testMatrix == cm$getOriginal()))
+  {
+    result <- c(result, TRUE)
+  } else
+  {
+    message("Unit 4, many1[", i, "]: Fail:")
+    message("Failed on base matrix", testMatrix)
+    result <- c(result, FALSE)
+  }
+  
+}
+
+if (all(result == c(TRUE)))
+{
+  message("Unit 4, many1: All ", maxruns, " tests passed.")
+} else
+{
+  message("Unit 4, many1: Some tests failed, see above messages.")
+}
+
+
+## Interestingly the integer matrix generator once came up with this almost-singular matrix,
+## showing that Solve() can choke on ill-conditioned matrices (condition number ~ 50,000 here!).
+##
+##    Original
+##    [,1] [,2] [,3]
+##    [1,]  109   54    9
+##    [2,]   12    6    1
+##    [3,]    2    5    1
+##
+##    Inverse
+##    [,1] [,2]          [,3]
+##    [1,]    1   -9 -4.106807e-15
+##    [2,]  -10   91 -1.000000e+00
+##    [3,]   48 -437  6.000000e+00
+##
+## but the coded inverse was the same as Solve()'s (i.e. both wrong), so the test passes.
+
+
+
+##### end units
+
+
+
 
